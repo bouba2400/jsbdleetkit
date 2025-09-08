c1891b34bb16f625720234dc50cfb0483e0a9f95
BD <bd@earth.net.test>
2025-09-08 15:55:30 -0400

    reformat

--- solution/bdleetkit/src/lib/list-utils.js
+++ solution/bdleetkit/src/lib/list-utils.js
@@ -10,18 +10,18 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-export const hasCycle = function(head) {
-    if (head === null) {
-        return false;
-    }
+export const hasCycle = function (head) {
+	if (head === null) {
+		return false;
+	}
 
-    let p1 = head.next;
-    let p2 = head.next ? head.next.next : null;
+	let p1 = head.next;
+	let p2 = head.next ? head.next.next : null;
 
-    while (p1 !== null && p2 !== null && p1 !== p2) {
-        p1 = p1.next;
-        p2 = p2.next ? p2.next.next : null;
-    }
+	while (p1 !== null && p2 !== null && p1 !== p2) {
+		p1 = p1.next;
+		p2 = p2.next ? p2.next.next : null;
+	}
 
-    return p1 !== null && p2 !== null && p1 === p2;
+	return p1 !== null && p2 !== null && p1 === p2;
 };
